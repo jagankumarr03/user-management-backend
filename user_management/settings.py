ad64588a065cfd70ed115856d3954c2947dd4e29
Jagankumar <jagankumarr03@gmail.com>
2026-01-03 14:21:41 +0530

    Switch database from Postgres to SQLite

--- user_management/settings.py
+++ user_management/settings.py
@@ -78,15 +78,10 @@ WSGI_APPLICATION = 'user_management.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'user_management_db',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
-    
 }
 
 
